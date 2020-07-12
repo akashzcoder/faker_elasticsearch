16b131edc5a3c9ff68bf6121cec1e9f0b2101d5a
Akash Singh <akashsingh09.03.93@gmail.com>
2020-07-11 23:03:34 -0400

    update to accept the config filepath

--- elasticsearch_data.py
+++ elasticsearch_data.py
@@ -3,9 +3,13 @@ import asyncio
 from es_test_data import CreateFakeData
 
 
-def create_fake_data(path_to_config: str = "./config/es.config"):
+def create_fake_data(path_to_config: str):
+    """
+    Create fake data is used to connect to the elasticsearch server, generate and store documents asynchronously.
+    The format of documents and the configuration of elasticsearch is read from the config file.
+
+    :param path_to_config: path to the configuration file.
+    :return: None
+    """
     c = CreateFakeData(path_to_config=path_to_config)
     asyncio.get_event_loop().run_until_complete(c.generate_test_data())
-
-
-create_fake_data()
